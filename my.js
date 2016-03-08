cb34ebd947ace59e6d04ea202e67cbc4d97854c7
sce <sce@scetekiimac.cn.ibm.com>
2016-03-08 16:49:07 +0800

    tt

--- my.js
+++ my.js
@@ -7,10 +7,60 @@ var data = ['河北', '秦皇岛', '54449', '54449.jpg', '2016 3-4 16:44:24', '2
 今日天气实况：气温:4℃;风向/风力:东风 3级;湿度:88%;紫外线强度:最弱。空气质量:很差', '-3℃/9℃', '3月5日 晴', '西北风转北风3-4级',
 			'0.gif', '0.gif', '-2℃/6℃', '3月6日 多云转阴', '无持续风向微风', '1.gif', '2.gif']
 
+var json =     {
+        "identifier": "PI-1-b7743e52-ae57-4552-9585-445195f527f6",
+        "model": "STP005",
+        "version": "SN16",
+        "state": "finished",
+        "outputs":
+        [
+            {
+                "name": "output1",
+                "data":
+                {
+                    "string":
+                    [
+                        "ºÓ±±",
+                        "ÌÆÉ½",
+                        "54534",
+                        "54534.jpg",
+                        "2016-3-8 13:46:06",
+                        "-7¡æ/5¡æ",
+                        "3ÔÂ8ÈÕ ¶àÔÆ",
+                        "±±·ç3-4¼¶×ªÎ¢·ç",
+                        "1.gif",
+                        "1.gif",
+                        "½ñÈÕÌìÆøÊµ¿ö£ºÆøÎÂ£º3¡æ£»·çÏò/·çÁ¦£ºÎ÷±±·ç 2¼¶£»Êª¶È£º14%£»×ÏÍâÏßÇ¿¶È£º×îÈõ¡£¿ÕÆøÖÊÁ¿£ºÁ¼¡£",
+                        "×ÏÍâÏßÖ¸Êý£º×îÈõ£¬·øÉäÈõ£¬Í¿²ÁSPF8-12·ÀÉ¹»¤·ôÆ·¡£",
+                        "-8¡æ/5¡æ",
+                        "3ÔÂ9ÈÕ ¶àÔÆ",
+                        "±±·ç3-4¼¶×ªÎ¢·ç",
+                        "1.gif",
+                        "1.gif",
+                        "-7¡æ/6¡æ",
+                        "3ÔÂ10ÈÕ Çç",
+                        "Î÷±±·ç3-4¼¶×ªÎ¢·ç",
+                        "0.gif",
+                        "0.gif",
+                        "ÌÆÉ½µØ´¦»·²³º£ÖÐÐÄµØ´ø£¬ÄÏ±õ²³º££¬±±ÒÀÑàÉ½£¬½»Í¨±ãÀû£¬¿É½ø³öÐÔºÜÇ¿¡£±³É½ÁÙº£µÄµØÀí¸ñ¾Ö¡¢¸´ÔÓ¶àÑùµÄµØÃ²ÀàÐÍºÍÌØÓÐµÄµØ·½ÀúÊ·ÎÄ»¯£¬Ôì¾ÍÁËÐí¶à¼«¾ßÌØÉ«µÄÂÃÓÎ×ÊÔ´¡£×ÔÈ»¾°¹ÛÆ·Î»¶ÀÌØ£¬ÈËÎÄ¾°¹ÛÎÄ»¯»ýµíÉîºñ¡£ÌÆÉ½±±²¿É½ÇøÓÐÃ÷³¤³Ç221¹«Àï£¬¶«½ÓÉ½º£¹Ø¡¢ÀÏÁúÍ·£¬Î÷½ÓÄ½ÌïÓø¡¢°Ë´ïÁë£¬ÓÐÃû¹ØÏÕ°¯29´¦£¬µÐÂ¥603×ù£¬·é»ðÌ¨82¸ö¡£Ë®ÏÂ³¤³Ç¡¢´óÀíÊ¯³¤³Ç¡¢72È¯Â¥¡¢¼àÓüÂ¥¡¢Ë®ÃÅ¡¢³¤³Ç×©Ò¤¡¢ÑøÂíÈ¦¡¢ÍÍ±øÓªµÈ½ÔÎª³¤³ÇÑØÏß¶ÀÐã£¬´ú±íÁËÃ÷³¤³ÇÀúÊ·ÎÄ»¯µÄ¾«»ª¡£³¤³ÇÑØÏßÒÑÓÐÇå¶«Áê¡¢¾°ÖÒÉ½¡¢ðÕ·åÉ½¡¢ÌÀÈª¡¢ÅË¼Ò¿Ú¡¢´óºÚÍ¡¡¢ÇàÉ½¹Ø¡¢ÁéÉ½¡¢°×ÑòÓøµÈÒ»ÏµÁÐÂÃÓÎ¾°Çø¡£Çå¶«ÁêÊ¼½¨ÓÚ¿µÎõ¶þÄê£¬ÊÇÄ¿Ç°ÎÒ¹úÏÖ´æ¹æÄ£ÅÓ´ó¡¢ÌåÏµÍêÕûµÄµÛÍõÁêÄ¹ÈºÖ®Ò»£¬2000Äê11ÔÂÁÐÎªÊÀ½çÎÄ»¯ÒÅ²ú¡£ÁêÇøÂñÔá×Å5¸ö»ÊµÛ¡¢15¸ö»Êºó¡¢137¸öåú×Ó¡¢4¸ö¹«Ö÷¡£Çå¶«ÁêµÄ½¨Öþ»Öºê¡¢×³¹Û¡¢¾«ÃÀ£¬ÅÓ´óµÄ½¨ÖþÈºÖÐÓÐÖÐ¹úÏÖ´æ×î´óµÄÊ¯ÅÆ·»£¬×î³¤µÄÉñÂ·¡£Î»ÓÚÇ¨Î÷ÏØ¾³ÄÚµÄ¾°ÖÒÉ½£¬ÒÔÆä²©´ó¾«Éî¡¢Ô´Ô¶Á÷³¤µÄ·ð¡¢µÀ¡¢ÈåÈý½ÌºÏÒ»µÄÈËÎÄºÍ×ÔÈ»¾°¹ÛÎÅÃûÓÚÊÀ£¬Ôø±»Çå¿µÎõ»ÊµÛÓùÌâ¡°ÁéÉ½ÐãÉ«¡±¡¢¡°ÌìÏÂÃûÉ½¡±¡£ÌÆÉ½ÓµÓÐÉ½¡¢º£¡¢ÁÖ¡¢µºµÈ¶àÖÖ¶À¾ßÌØÉ«µÄ×ÔÈ»¾°¹Û,¾³ÄÚÓÐ¹ú¼Ò¼¶ÎÄÎï±£»¤µ¥Î»2´¦£¬Ê¡¼¶ÖØµãÎÄÎï±£»¤µ¥Î»40´¦¡£ÌÆÉ½ÓÐ196.5¹«Àïº£°¶Ïß£¬º£±õ·ç¹âÐãÀö£¬ÓÈÆäÊÇÆÐÌáµº¡¢ÔÂÛçµº¡¢´òÍø¸ÚÈý¸ö½üº£µºÓì£¬ÕýÔÚ¿ª·¢½¨ÉèÒÔ×ÔÈ»ÉúÌ¬¹Û¹âºÍÐÝÏÐ¶È¼ÙÎªÖ÷ÒªÄÚÈÝµÄÐÂÐÍÂÃÓÎÇø£¬³ÉÎª»ª±±µØÇøÌØÉ«ÂÃÓÎö¦µã¡£"
+                    ]
+                }
+            }
+        ],
+        "container_name": "WJTA",
+        "creation_time": "2016-03-08T05:52:53Z",
+        "stopped_flow_objects_count": 0,
+        "all_stopped_flow_objects_count": 0,
+        "top_level": true
+    }
+
+
 function findData(outputs, name) {
-	for(var output in outputs) {
-		if (output['name'] === name) {
-			return output['data'];
+	var i;
+	for(i in outputs) {
+		console.info(outputs[i]);
+		if (outputs[i]['name'] === name) {
+			return outputs[i]['data'];
 		}
 	}
 }
@@ -19,20 +69,26 @@ function check() {
 	var city = $('input.city').val(),
 		date = $('input.date').val();
 
-	$.ajax({
-		async: false,
-		url: "http://sou.qq.com/online/get_weather.php?callback=Weather&city=nanjing",
-		type: "GET",
-		dataType: 'json',
-		timeout: 5000,
-		success: function (json) {//客户端jquery预先定义好的callback函数,成功获取跨域服务器上的json数据后,会动态执行这个callback函数
-		   	if(json.actionErrors.length!=0){
-		   		alert(json.actionErrors);
-		   	}
-	   		data = findData(json['outputs'], 'output1')['string'];
-	   		showWeather();
-		}
-	});
+	// $.ajax({
+	// 	async: false,
+	// 	url: "http://sou.qq.com/online/get_weather.php?callback=Weather&city=nanjing",
+	// 	type: "GET",
+	// 	dataType: 'json',
+	// 	timeout: 5000,
+	// 	success: function (json) {//客户端jquery预先定义好的callback函数,成功获取跨域服务器上的json数据后,会动态执行这个callback函数
+	// 	   	if(json.actionErrors.length!=0){
+	// 	   		alert(json.actionErrors);
+	// 	   	}
+	//    		data = findData(json['outputs'], 'output1')['string'];
+	//    		showWeather();
+	// 	},
+	// 	error: function (json) {//客户端jquery预先定义好的callback函数,成功获取跨域服务器上的json数据后,会动态执行这个callback函数
+	// 	   	alert('无法查到您所输入的城市，请修改或更正城市名称');
+	// 	}
+	// });
+
+	data = findData(json['outputs'], 'output1')['string'];
+	showWeather();
 }
 
 function showWeather() {
@@ -42,6 +98,9 @@ function showWeather() {
 	$('.tips').html(data[11]);
 
 	var tr, th, td, img, arrow;
+
+	$('.table').html('');
+
 
 	tr = $('<tr></tr>');
 	th = $('<th></th>');
@@ -113,7 +172,25 @@ function showWeather() {
 }
 
 function analyze() {
-	var feedback = $('#feedback');
+	var feedback = $('#feedback').val();
+
+	// $.ajax({
+	// 	async: false,
+	// 	url: "http://sou.qq.com/online/get_weather.php?callback=Weather&city=nanjing",
+	// 	type: "GET",
+	// 	dataType: 'json',
+	// 	timeout: 5000,
+	// 	success: function (json) {//客户端jquery预先定义好的callback函数,成功获取跨域服务器上的json数据后,会动态执行这个callback函数
+	// 	   	if(json.actionErrors.length!=0){
+	// 	   		alert(json.actionErrors);
+	// 	   	}
+	//    		data = findData(json['outputs'], 'output1')['string'];
+	//    		showWeather();
+	// 	}
+	// });
+
+	$('.modal strong').html(feedback);
+	$('.modal').modal();
 }
 
 
